644cf67b3503b81742c1dc65e53e2f319d5dab70
Adrian Gonzalez <g.adrian.gonzalez.90@gmail.com>
2023-04-10 21:34:58 -0600

    Added frontend clientes project

---
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,18 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 2, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 3, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 4, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 5, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 6, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 7, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 8, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 9, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 10, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 11, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 12, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 13, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'},
+  {id: 14, nombre: 'Adrián', apellido: 'González', email:'adrian@mail.com', createAt: '2017-12-11'}
+];
