5988c9002b8299364973805514574bc99a18947b
Adrian Gonzalez <g.adrian.gonzalez.90@gmail.com>
2023-04-11 21:26:14 -0600

    Added frontend functionality for CRUD operations

--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from "./cliente";
 import { ClienteService } from "./cliente.service";
+import swal from "sweetalert2";
 
 @Component({
   selector: 'app-clientes',
@@ -15,5 +16,32 @@ export class ClientesComponent implements OnInit{
 
   ngOnInit(): void {
     this.clienteService.getClientes().subscribe(clientes => this.clientes = clientes);
+    //this.clientes = [];
+  }
+
+  delete(cliente: Cliente) : void {
+    swal({
+      title: '¿Estás seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli != cliente);
+            swal(
+              '¡Cliente Eliminado!',
+              `Cliente ${cliente.nombre} eliminado con éxito`,
+              'success'
+            );
+          }
+        );
+      }
+    })
   }
 }
