fbfee1ae9eae915d45520e5e896280324a7668af
Adrian Gonzalez <g.adrian.gonzalez.90@gmail.com>
2023-05-04 20:40:25 -0600

    Completing the app to deploy

--- spring-boot-backend-apirest/src/main/java/com/driagon/springboot/backend/apirest/app/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/driagon/springboot/backend/apirest/app/controllers/ClienteRestController.java
@@ -24,7 +24,7 @@ import java.net.MalformedURLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200", "*"})
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteRestController {
